ff69ba06bd555e710332407410b2b3bbfe3e1788
Nalinkumar Manikandan <75604279+nalinmanikandan@users.noreply.github.com>
2024-01-08 19:49:02 +0530

    Add files via upload

---
+++ src/main/java/com/backend2/productreview/dto/CommentDto.java
@@ -0,0 +1,15 @@
+package com.backend2.productreview.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDto {
+    private Long id;
+    private String comment;
+}
